d83559711cf3f6e827a1e317b3b4367f204fdb59
Naoto Maeda <aos81922710@yahoo.co.jp>
2018-12-10 14:18:01 +0900

    add 05 directory

---
+++ 05/02-jsx/src/App.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    const logoOptions = {
+      alt: "logo",
+      className: "App-logo",
+      src: logo
+    };
+    const title = "こんにちはReact";
+    const targets = ["World", "Kanae", "Yukina"];
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          { // コメントはこう書く
+
+          }
+          <img {...logoOptions} />
+          {}
+          {title && <h1 className="App-title">{title}</h1>}
+        </header>
+        {targets.map(target => (
+          <p className="App-intro">Hello, {target}</p>
+        ))}
+      </div>
+    );
+  };
+
+}
+
+export default App;
