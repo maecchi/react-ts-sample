b3a1aac6be9d5742e5969aa4cf8541d3c19476cc
Naoto Maeda <aos81922710@yahoo.co.jp>
2018-12-11 15:05:12 +0900

    add 10 directory

---
+++ 10/04-fsa/src/containers/Counter.tsx
@@ -0,0 +1,43 @@
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+import { Action } from 'typescript-fsa';
+
+import {
+  add,
+  CounterActionPayload,
+  decrement,
+  increment,
+} from 'actions/counter';
+import Counter, { CounterProps } from 'components/Counter';
+import { State } from 'reducer';
+
+interface StateProps {
+  count: number;
+}
+
+interface DispatchProps {
+  add: (amount: number) => void;
+  decrement: () => void;
+  increment: () => void;
+}
+
+const mapStateToProps = (state: State): StateProps => ({
+  count: state.count,
+});
+
+const mapDispatchToProps = (
+  dispatch: Dispatch<Action<CounterActionPayload>>, // 型引数のActionを書き換え
+): DispatchProps =>
+  bindActionCreators(
+    {
+      decrement,
+      increment,
+      add: (amount: number) => add({ amount }), // `add()`の呼び方を書き換え
+    },
+    dispatch,
+  );
+
+export default connect<StateProps, DispatchProps, CounterProps>(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Counter);
