fc63a884874fe1b10daed36f5d804703d01507d5
Naoto Maeda <aos81922710@yahoo.co.jp>
2018-12-13 13:52:06 +0900

    copying 05-functional to 05-functional-copying

--- 07/05-functional-copying/src/App.tsx
+++ 07/05-functional-copying/src/App.tsx
@@ -1,22 +1,34 @@
 import * as React from 'react';
+import CharacterList, { Character } from './CharacterList';
+
 import './App.css';
 
-import logo from './logo.svg';
+const App: React.SFC<{}> = () => {
+  const characters: Character[] = [
+    {
+      name: '羽咲 綾乃',
+      age: 16,
+      height: 151,
+    },
+    {
+      name: '荒垣 なぎさ',
+      age: 18,
+      height: 174,
+    },
+    {
+      name: '泉 理子',
+      age: 18,
+    },
+  ];
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container">
+      <header>
+        <h1>はねバド！ キャラクター一覧</h1>
+      </header>
+      <CharacterList school="北小町高校" characters={characters} />
+    </div>
+  );
+};
 
 export default App;
